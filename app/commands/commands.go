5edc6652e00896b9209b05e6a54e8a6b98df8794
CorentinGS <contact@corentings.dev>
2025-03-26 16:36:51 +0100

    add matcha and beer commands with asynchronous response handling

--- app/commands/commands.go
+++ app/commands/commands.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"context"
+	"math/rand"
 	"time"
 
 	"github.com/corentings/kafejo-bot/app/commands/versioncommand"
@@ -17,6 +18,8 @@ import (
 var commands = []api.CreateCommandData{
 	{Name: "ping", Description: "Ping!"},
 	{Name: "coffee", Description: "Get a cup of coffee"},
+	{Name: "matcha", Description: "Get a cup of matcha"},
+	{Name: "beer", Description: "Get a cup of beer"},
 	versioncommand.GetCommandData(),
 }
 
@@ -28,6 +31,9 @@ func NewHandler(s *state.State) *handler.CommandHandler {
 	h := handler.CreateHandler(s)
 	h.AddFunc("ping", pingCommandHandler())
 	h.AddFunc("coffee", CoffeeCommandHandler())
+	h.AddFunc("matcha", MatchaCommandHandler())
+	h.AddFunc("beer", BeerCommandHandler())
+
 	registerVersion(h)
 	return h
 }
@@ -39,6 +45,44 @@ type Command struct {
 func pingCommandHandler() cmdroute.CommandHandlerFunc {
 	return func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
 		return &api.InteractionResponseData{Content: option.NewNullableString("Pong!")}
+	}
+}
+
+// GetRandomInt generates a random number between min and max
+func GetRandomInt(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func BeerCommandHandler() cmdroute.CommandHandlerFunc {
+	var beers = []string{"https://tenor.com/view/schwanchi-gif-10646964323290635570", "https://tenor.com/view/beer-garden-beer-waitress-gif-14167773785568845141"}
+	return func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
+		go func() {
+			time.Sleep(10 * time.Second)
+			// get a random beer gif
+			beergif := beers[GetRandomInt(0, len(beers))]
+			_, err := utils.GetSession().SendMessage(data.Event.ChannelID,
+				"Your beer is ready "+data.Event.Member.Mention()+"! :beers:\n"+beergif)
+			if err != nil {
+				return
+			}
+		}()
+		return &api.InteractionResponseData{Content: option.NewNullableString("Your order has been taken, please wait while we pour your beer!"), Flags: discord.EphemeralMessage}
+	}
+}
+
+func MatchaCommandHandler() cmdroute.CommandHandlerFunc {
+	return func(ctx context.Context, data cmdroute.CommandData) *api.InteractionResponseData {
+		// Make a goroutine to send the message in the queue after 10 seconds to simulate the matcha brewing
+		go func() {
+			time.Sleep(10 * time.Second)
+			_, err := utils.GetSession().SendMessage(data.Event.ChannelID,
+				"Your matcha is ready "+data.Event.Member.Mention()+"! :tea:\nhttps://tenor.com/view/tea-matcha-green-tea-stir-matcha-green-tea-gif-17550598")
+			if err != nil {
+				return
+			}
+		}()
+
+		return &api.InteractionResponseData{Content: option.NewNullableString("Your order has been taken, please wait while we brew your matcha!"), Flags: discord.EphemeralMessage}
 	}
 }
 
